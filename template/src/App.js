a8d96abb216b3e75cb43df1327542b2f57ed084f
codykarunas <codykarunas@gmail.com>
2020-01-07 14:56:55 -0500

    Initial commit.

---
+++ template/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './containers/Home'
+
+const App = () => {
+  return (
+    <div>
+      CRA Template initialised... happy coding!
+      <Home />
+    </div>
+  );
+}
+
+export default App;
