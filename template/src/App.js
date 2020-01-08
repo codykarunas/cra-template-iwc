71f18239b5cc551da41b69b58f045d718e3b8aff
codykarunas <codykarunas@gmail.com>
2020-01-08 14:05:27 -0500

    Setting up routes.

--- template/src/App.js
+++ template/src/App.js
@@ -5,31 +5,17 @@ import {
   Switch,
   Link
 } from 'react-router-dom'
-
+import * as ROUTES from './constants/routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './containers/Home'
-import About from './containers/About'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
 
 const App = () => {
   return (
     <Router>
-      <Container>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <div>CRA Template initialised... happy coding!</div>
-        <Button>
-          Sign In
-        </Button>
-        <Button>
-          Sign Up
-        </Button>
-      </Container>
       <Route>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
+          <Route exact path={ROUTES.HOME} component={Home} />
         </Switch>
       </Route>
     </Router>
