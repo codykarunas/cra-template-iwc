4039441f3c9067a2eac03f2f558b4437bee01a28
codykarunas <codykarunas@gmail.com>
2020-01-08 13:30:04 -0500

    Added react bootstrap.

--- template/src/App.js
+++ template/src/App.js
@@ -3,20 +3,33 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Link,
-  withRouter,
+  Link
 } from 'react-router-dom'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './containers/Home'
+import About from './containers/About'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 const App = () => {
   return (
     <Router>
-      <div>CRA Template initialised... happy coding!</div>
-      <Link to="/">Home</Link>
+      <Container>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <div>CRA Template initialised... happy coding!</div>
+        <Button>
+          Sign In
+        </Button>
+        <Button>
+          Sign Up
+        </Button>
+      </Container>
       <Route>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
         </Switch>
       </Route>
     </Router>
